d70e872d4598b5f90da0259353348506f0125b7e
Saurabh Gupta <saurabhgupta9369899665@gmail.com>
2023-05-24 23:48:55 +0530

    .

--- love babar/priority_queue.cpp
+++ love babar/priority_queue.cpp
@@ -6,8 +6,7 @@ int main(){
    priority_queue<int> maxi;           //max is_heap...
 
    priority_queue<int,vector<int>, greater<int> >mini;
-
-   maxi.push(1);
+  maxi.push(1);
    maxi.push(3);
    maxi.push(2);
    maxi.push(0);
@@ -16,7 +15,6 @@ int main(){
    for(int i=0;i<n;i++){
        cout<<maxi.top()<<" ";
        maxi.pop();
-    
    }cout<<endl;
    mini.push(3);
    int m= mini.size();
